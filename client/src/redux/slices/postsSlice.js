3da714fc943eefb5fcb183e39f056aae265ae433
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-08-01 18:43:35 +0300

    upd: added logic receiving posts && tags

---
+++ client/src/redux/slices/postsSlice.js
@@ -0,0 +1,45 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "../../axios";
+
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+  const { data } = await axios.get("posts");
+  return data;
+});
+
+export const fetchTags = createAsyncThunk("posts/fetchTags", async () => {
+  const { data } = await axios.get("tags");
+  return data;
+});
+
+const initialState = {
+  posts: {
+    items: [],
+    status: "loading",
+  },
+  tags: {
+    items: [],
+    status: "loading",
+  },
+};
+
+const postsSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchPosts.pending]: (state) => {
+      state.posts.items = [];
+      state.posts.status = "loading";
+    },
+    [fetchPosts.fulfilled]: (state, action) => {
+      state.posts.items = action.payload;
+      state.posts.status = "loaded";
+    },
+    [fetchPosts.rejected]: (state) => {
+      state.post.items = [];
+      state.posts.status = "error";
+    },
+  },
+});
+
+export default postsSlice.reducer;
